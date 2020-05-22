9e03f17abe3b8539156d2052252d43327aff385e
Dominique Maack <dominiquemaack@Dominiques-MBP.domain>
2020-05-22 16:37:05 -0600

    storing user data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,40 @@ const config = {
     messagingSenderId: "718663385721",
     appId: "1:718663385721:web:2f3fc7254c8e114a7b8aa8",
     measurementId: "G-JTFD2TEPSS"
-  };
+};
+
+// passing in the userAuth object I see in the console.log()
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    // checking if there is any data in the snapShot - if not, create one
+    if(!snapShot.exists) {
+        // deconstructing for the displayname and email
+        const { displayName, email } = userAuth;
+        // tells us the current date and time this was invoked
+        const createdAt = new Date();
+
+        try {
+            // creating user
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (err) {
+            console.log('error creating user', err.message);
+        }
+    }
+
+    
+    return userRef;
+
+}
 
 firebase.initializeApp(config);
 
