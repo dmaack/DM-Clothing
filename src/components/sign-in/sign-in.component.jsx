169b7ecb0e0631179a2017b166a5453600c3b487
Dominique Maack <dominiquemaack@Dominiques-MBP.domain>
2020-05-22 23:14:11 -0600

    added signup component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,6 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
-import CustomButtom from '../custom-button/custom-button.component';
 
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 
@@ -53,8 +52,8 @@ class SignIn extends React.Component {
                         required />
                     
                     <div className='buttons'>
-                        <CustomButtom type='submit'>Sign in</CustomButtom>
-                        <CustomButtom onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButtom>
+                        <CustomButton type='submit'>Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
