09c0818702dbd147eec46a547f33085e1b22ba87
Dominique Maack <--system>
2020-05-29 12:19:12 -0600

    config4

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -10,19 +10,19 @@ const CheckoutPage = ({ cartItems, total}) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
             <div className='header-block'>
-                <span>Product </span>
+                <span>Product</span>
             </div>
             <div className='header-block'>
-                <span>Description </span>
+                <span>Description</span>
             </div>
             <div className='header-block'>
-                <span>Quantity </span>
+                <span>Quantity</span>
             </div>
             <div className='header-block'>
-                <span>Price </span>
+                <span>Price</span>
             </div>
             <div className='header-block'>
-                <span>Remove </span>
+                <span>Remove</span>
             </div>
         </div>
         {
