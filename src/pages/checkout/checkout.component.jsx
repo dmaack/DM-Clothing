092009e0b8a1e208728ac76fd3aae64915ffc13d
dmaack <--system>
2020-05-29 12:27:43 -0600

    check

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -19,7 +19,7 @@ const CheckoutPage = ({ cartItems, total}) => (
                 <span>Quantity</span>
             </div>
             <div className='header-block'>
-                <span>Price</span>
+                <span>Price </span>
             </div>
             <div className='header-block'>
                 <span>Remove</span>
