e8df26165830ff9c32b0ada55de8dd36a4a8e029
Dominique Maack <dominiquemaack@gmail.com>
2020-07-16 23:30:08 -0600

    added new dependency for persisted local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // we want to use local storage as default storage
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers ({
+const persistConfig = {
+    key: 'root',
+    storage, 
+    whitelist: ['cart'] // the only thing we want to persist is the cart
+}
+
+const rootReducer = combineReducers ({
     user: userReducer,
     cart: cartReducer
 });
+
+// Modified version of rootReducer and the persistance capabalities from redux-persist
+export default persistReducer(persistConfig, rootReducer);
