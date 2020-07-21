dc480ad56b96730406100d9d8f81ed70903baf58
Dominique Maack <dominiquemaack@gmail.com>
2020-07-21 14:25:16 -0600

    destructored section state to it's own reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage'; // we want to use local storage
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +14,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers ({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
 
 // Modified version of rootReducer and the persistance capabalities from redux-persist
